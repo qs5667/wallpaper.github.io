7865f127d95c3731ff48e9121261b72002a8ce64
qs5667 <148621021+qs5667@users.noreply.github.com>
2024-07-30 20:34:14 +0800

    Update BingWallpaperDownloader.java

--- BingWallpaperDownloader.java
+++ BingWallpaperDownloader.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import org.json.JSONObject;
@@ -9,6 +10,7 @@ import org.json.JSONObject;
 public class BingWallpaperDownloader {
     private static final String BING_API_URL = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US";
     private static final String BING_BASE_URL = "https://www.bing.com";
+    private static final String DOWNLOAD_DIR = "wallpapers"; // 下载文件夹
 
     public static void main(String[] args) throws IOException {
         String imageUrl = getImageUrl();
@@ -26,11 +28,20 @@ public class BingWallpaperDownloader {
     }
 
     private static void downloadImage(String imageUrl) throws IOException {
+        // 创建下载文件夹如果它不存在
+        Path downloadPath = Paths.get(DOWNLOAD_DIR);
+        if (Files.notExists(downloadPath)) {
+            Files.createDirectories(downloadPath);
+        }
+
         URL url = new URL(imageUrl);
         String fileName = "bing_wallpaper_" + System.currentTimeMillis() + ".jpg";
+        Path filePath = downloadPath.resolve(fileName);
+
         try (InputStream in = url.openStream()) {
-            Files.copy(in, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(in, filePath, StandardCopyOption.REPLACE_EXISTING);
         }
-        System.out.println("Wallpaper downloaded: " + fileName);
+
+        System.out.println("Wallpaper downloaded: " + filePath);
     }
 }
